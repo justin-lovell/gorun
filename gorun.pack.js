b542e352e9f75fa922eac9ebdb2eaffa97f6acf3
Justin Lovell <justin@justjuzzy.com>
2017-02-08 19:39:51 +1100

    getting the hang of npm pub

--- gorun.pack.js
+++ gorun.pack.js
@@ -1,16 +1,27 @@
 'use strict';
 
+
+const copyJavaScript = {
+  initialize: [{
+    source: 'lib/**/*.js',
+    destination: 'dist'
+  }],
+  transform: require('./lib/transform-babel')
+};
+
+const copyPackageJson = {
+  initialize: [{
+    source:'package.json',
+    destination: 'dist'
+  }]
+};
+
 export default {
   directoriesToClean: () => [
     'dist/*'
   ],
   copy: [
-    {
-      initialize: [{
-        source: 'lib/**/*.js',
-        destination: 'dist'
-      }],
-      transform: require('./lib/transform-babel')
-    }
+    copyJavaScript,
+    copyPackageJson
   ]
 };
